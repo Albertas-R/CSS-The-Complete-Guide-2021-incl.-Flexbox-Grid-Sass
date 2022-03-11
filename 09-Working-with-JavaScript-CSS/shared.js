011519c464da6e3facd8e4b0c24246b6f093f87b
Albertas <albertas.rinkevicius@gmail.com>
2022-03-11 12:50:02 +0200

    09 end

--- 09-Working-with-JavaScript-CSS/shared.js
+++ 09-Working-with-JavaScript-CSS/shared.js
@@ -1,11 +1,41 @@
-var backdrop = document.querySelector('.backdrop');
-var modal = document.querySelector('.modal');
-var selectPlanButtons = document.querySelectorAll('.plan button');
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+const modalNoButton = document.querySelector(".modal__action--negative");
+const toggleButton = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
 
-// console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
-    selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
-    });
-}   
+  selectPlanButtons[i].addEventListener("click", function () {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    backdrop.classList.add("open");
+    modal.classList.add("open");
+  });
+}
+
+const closeModal = function () {
+  // modal.style.display = "none";
+  // backdrop.style.display = "none";
+  backdrop.classList.remove("open");
+  if (modal) {
+    modal.classList.remove("open");
+  }
+};
+
+backdrop.addEventListener("click", function () {
+  closeModal();
+  // mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
+});
+
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
+}
+
+toggleButton.addEventListener("click", function () {
+  // mobileNav.style.display = "block";
+  // backdrop.style.display = "block";
+  backdrop.classList.add("open");
+  mobileNav.classList.add("open");
+});
